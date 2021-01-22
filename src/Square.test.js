67faddad0a17f63a7be58b228ea4f9982dd2f9a8
Kiran Pinnipati <kpinnipati@gmail.com>
2021-01-21 18:46:37 -0800

    Finished up to section 4

--- src/Square.test.js
+++ src/Square.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 // Square is a "default" export from Square.js, so we don't need
 // curly braces around it like we do for render
@@ -14,4 +14,12 @@ test('should render a Square with X', () => {
     const square = render(<Square value={'X'} />); // Line 1
     const button = square.getByRole('button'); // Line 2 -- The "role" here is according to the aria-role standard
     expect(button.innerHTML).toBe('X'); // Line 3
+});
+
+test('should call the specified onClick when square is clicked', () => {
+    const onClick = jest.fn();
+    const square = render(<Square value='X' onClick={onClick} />);
+    const button = square.getByRole('button');
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalledTimes(1);
 });
